bae9f3af1f2c69633ccbee2c0e135b957682b5ca
Prashasta Srivastava <147490436+PrashastS1@users.noreply.github.com>
2025-04-03 14:36:29 +0530

    added 2 new builtins, fg and jobs

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -3,6 +3,8 @@
 struct builtin_s builtins[] =
 {   
     { "dump"    , dump       },
+    { "jobs",    shell_jobs  },
+    { "fg",      shell_fg    },
 };
 
 int builtins_count = sizeof(builtins)/sizeof(struct builtin_s);
